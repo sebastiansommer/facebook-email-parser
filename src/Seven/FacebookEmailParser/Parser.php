9584f3b80800c7c037c232f1ef4709fb9cf57b62
Sebastian Sommer <mail@sebastian-sommer.com>
2019-06-26 14:30:52 +0800

    Initial commit

---
+++ src/Seven/FacebookEmailParser/Parser.php
@@ -0,0 +1,36 @@
+<?php
+namespace Seven\FacebookEmailParser;
+
+class Parser
+{
+    /**
+     * @var string
+     */
+    protected $content;
+
+    public function __construct(string $content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccountId(): string
+    {
+        return $this->extractOption('Account Id');
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    protected function extractOption(string $name): string
+    {
+        $regex = '/' . $name . ': (.*)[\n|\r\n|\r]{2}/';
+
+        preg_match_all($regex, $this->content, $matches, PREG_SET_ORDER, 0);
+
+        return $matches[0][1];
+    }
+}
